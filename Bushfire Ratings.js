77a9c2a813b9de7aaf3055a42bc21a16c41d4772
7a2r1c3 <134197279+7a2r1c3@users.noreply.github.com>
2023-10-16 09:49:33 +0800

    Add files via upload

---
+++ Bushfire Ratings.js
@@ -0,0 +1,45 @@
+function toggleText1() {
+  var text1 = document.getElementById("test1");
+  if (text1.style.display === "none") {
+    text1.style.display = "block";
+  } else {
+    text1.style.display = "none";
+  }
+}
+
+function toggleText2() {
+  var text2 = document.getElementById("test2");
+  if (text2.style.display === "none") {
+    text2.style.display = "block";
+  } else {
+    text2.style.display = "none";
+  }
+}
+
+function toggleText3() {
+  var text3 = document.getElementById("test3");
+  if (text3.style.display === "none") {
+    text3.style.display = "block";
+  } else {
+    text3.style.display = "none";
+  }
+}
+
+function toggleText4() {
+  var text4 = document.getElementById("test4");
+  if (text4.style.display === "none") {
+    text4.style.display = "block";
+  } else {
+    text4.style.display = "none";
+  }
+}
+
+
+function toggleText5() {
+  var text5 = document.getElementById("test5");
+  if (text5.style.display === "none") {
+    text5.style.display = "block";
+  } else {
+    text5.style.display = "none";
+  }
+}
